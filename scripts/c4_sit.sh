4c93c86297e26b22ed5c995d5081a495a6aff19c
Mike Loeffler <michael.t.loeffler@gmail.com>
2021-08-19 12:03:31 -0400

    Corrected math

--- scripts/c4_sit.sh
+++ scripts/c4_sit.sh
@@ -4,10 +4,10 @@
 
 if [ $1 ] && [ "$2" = "set" ]
 then
-  COMMAND=$(($1 + 29))
+  COMMAND=$(($1 * 2 + 28))
 elif [ $1 ] && [ "$2" = "call" ]
 then
-  COMMAND=$(($1 + 30))
+  COMMAND=$(($1 * 2 + 29))
 fi
 
 #echo "$CAM4_URL/decoder_control.cgi?user=$CAM4_USR&pwd=$CAM4_PWD&command=$COMMAND&onestep=0&sit=$COMMAND"
