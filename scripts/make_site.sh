7f67ff7387a60a4a22066f9c14669dfd10ee7356
Mike Loeffler <mtl010957@comcast.net>
2022-01-14 10:53:19 -0500

    Rework cameras to use latest gallery card capabilities

--- scripts/make_site.sh
+++ scripts/make_site.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo "Cam1 Move Snaps..." && cd /config/www/3629cam1/C1_00626E69269A/snap && /config/scripts/move_snaps.sh
-echo "Cam1 Move Records..." && cd /config/www/3629cam1/C1_00626E69269A/record && /config/scripts/move_records.sh
+echo "Cam1 Move Snaps..." && cd /config/www/3629cam1/3629cam1_snap && /config/scripts/move_snaps.sh
+echo "Cam1 Move Records..." && cd /config/www/3629cam1/3629cam1_record && /config/scripts/move_records.sh
 
-echo "Cam2 Move Snaps..." && cd /config/www/3629cam2/C1_00626E6926B6/snap && /config/scripts/move_snaps.sh
-echo "Cam2 Move Records..." && cd /config/www/3629cam2/C1_00626E6926B6/record && /config/scripts/move_records.sh
+echo "Cam2 Move Snaps..." && cd /config/www/3629cam2/3629cam2_snap && /config/scripts/move_snaps.sh
+echo "Cam2 Move Records..." && cd /config/www/3629cam2/3629cam2_record && /config/scripts/move_records.sh
 
-echo "Cam3 Move Snaps..." && cd /config/www/3629cam3/C1_00626E82585D/snap && /config/scripts/move_snaps.sh
-echo "Cam3 Move Records..." && cd /config/www/3629cam3/C1_00626E82585D/record && /config/scripts/move_records.sh
+echo "Cam3 Move Snaps..." && cd /config/www/3629cam3/3629cam3_snap && /config/scripts/move_snaps.sh
+echo "Cam3 Move Records..." && cd /config/www/3629cam3/3629cam3_record && /config/scripts/move_records.sh
 
-echo "Cam4 Move JPEGs..." && cd /config/www/3629cam4 && /config/scripts/move_cam4_jpeg.sh
-echo "Cam4 Get MP4s..." && cd /config/www/3629cam4 && /config/scripts/get_cam4_mp4.sh
+echo "Cam4 Move JPEGs..." && cd /config/www/3629cam4/3629cam4_snap && /config/scripts/move_cam4_jpeg.sh
+echo "Cam4 Get MP4s..." && cd /config/www/3629cam4/3629cam4_record && /config/scripts/get_cam4_mp4.sh
--- scripts/move_cam4_jpeg.sh
+++ scripts/move_cam4_jpeg.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mv ../*.jpg .
 ls *.jpg | awk -F_ '{printf("%s\n",substr($3,1,8))}' | sort -u > ../dates
 for dir in `cat ../dates`; do mkdir -p ${dir}; done
 for dir in `cat ../dates`; do mv *_${dir}??????_?.jpg ${dir}; done
