dcdd2efa8a47f499be156636902daa30dfc33252
Mike Loeffler <mtl010957@comcast.net>
2023-06-20 13:12:42 -0400

    Removed obsolete camera

--- scripts/c1_md.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. /config/shell_secrets.sh
-
-curl -k --silent "$CAM1_URL/cgi-bin/CGIProxy.fcgi?cmd=getDevState&usr=$CAM1_USR&pwd=$CAM1_PWD" | grep -oP "(?<=motionDetectAlarm>).*?(?=</motionDetectAlarm>)"
-exit 0
-
