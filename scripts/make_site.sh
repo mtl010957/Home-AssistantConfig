da3f94d3bf69ef71c3287b5fb059bbc531f2912e
Mike Loeffler <mtl010957@comcast.net>
2023-12-12 15:34:13 -0500

    Handle both h264 and h265 files

--- scripts/make_site.sh
+++ scripts/make_site.sh
@@ -7,3 +7,5 @@ echo "Cam6 Get Files..." && /config/scripts/get_cam_files.sh ${datedir} /media/3
 echo "Cam8 Get Files..." && /config/scripts/get_cam_files.sh ${datedir} /media/3629cam8 ${USR_3629CAM8} ${PWD_3629CAM8} ${URL_3629CAM8}
 echo "Cam6 Make MP4..." && /config/scripts/h264_to_mp4.sh ${datedir} 3629cam6
 echo "Cam8 Make MP4..." && /config/scripts/h264_to_mp4.sh ${datedir} 3629cam8
+echo "Cam6 Make MP4..." && /config/scripts/h265_to_mp4.sh ${datedir} 3629cam6
+echo "Cam8 Make MP4..." && /config/scripts/h265_to_mp4.sh ${datedir} 3629cam8
