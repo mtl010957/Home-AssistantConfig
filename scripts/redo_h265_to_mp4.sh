bce9c18426fbefee923e4bfecbee66cfa0d308da
Mike Loeffler <mtl010957@comcast.net>
2023-12-17 10:44:16 -0500

    Adjust redo scripts for easier use

--- scripts/redo_h265_to_mp4.sh
+++ scripts/redo_h265_to_mp4.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 . /config/shell_secrets.sh
-datedir=20231116
 
-/config/scripts/get_cam_265_records.sh ${datedir} /media/3629cam6 ${USR_3629CAM6} ${PWD_3629CAM6} ${URL_3629CAM6} && /config/scripts/h265_to_mp4.sh ${datedir} 3629cam6
-/config/scripts/get_cam_265_records.sh ${datedir} /media/3629cam8 ${USR_3629CAM8} ${PWD_3629CAM8} ${URL_3629CAM8} && /config/scripts/h265_to_mp4.sh ${datedir} 3629cam8
+for datedir in `cat /config/scripts/redo_dates.txt`
+do
+  /config/scripts/get_cam_265_records.sh ${datedir} /media/3629cam6 ${USR_3629CAM6} ${PWD_3629CAM6} ${URL_3629CAM6} && \
+  /config/scripts/h265_to_mp4.sh ${datedir} 3629cam6
+  /config/scripts/get_cam_265_records.sh ${datedir} /media/3629cam8 ${USR_3629CAM8} ${PWD_3629CAM8} ${URL_3629CAM8} && \
+  /config/scripts/h265_to_mp4.sh ${datedir} 3629cam8
+done
