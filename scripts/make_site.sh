837ecbd5c4a61a1b6b4372278bbc5eeb4725cefd
Mike Loeffler <mtl010957@comcast.net>
2024-01-23 21:52:32 -0500

    Add 13821cam1

--- scripts/make_site.sh
+++ scripts/make_site.sh
@@ -3,10 +3,13 @@
 . /config/shell_secrets.sh
 datedir=`date +%Y%m%d`
 
+echo "Cam1 Get Files..." && /config/scripts/get_cam_files.sh ${datedir} /media/13821cam1 ${USR_13821CAM1} ${PWD_13821CAM1} ${URL_13821CAM1}
 echo "Cam6 Get Files..." && /config/scripts/get_cam_files.sh ${datedir} /media/3629cam6 ${USR_3629CAM6} ${PWD_3629CAM6} ${URL_3629CAM6}
 echo "Cam8 Get Files..." && /config/scripts/get_cam_files.sh ${datedir} /media/3629cam8 ${USR_3629CAM8} ${PWD_3629CAM8} ${URL_3629CAM8}
+echo "Cam1 Make MP4..." && /config/scripts/h264_to_mp4.sh ${datedir} 13821cam1
 echo "Cam6 Make MP4..." && /config/scripts/h264_to_mp4.sh ${datedir} 3629cam6
 echo "Cam8 Make MP4..." && /config/scripts/h264_to_mp4.sh ${datedir} 3629cam8
+echo "Cam1 Make MP4..." && /config/scripts/h265_to_mp4.sh ${datedir} 13821cam1
 echo "Cam6 Make MP4..." && /config/scripts/h265_to_mp4.sh ${datedir} 3629cam6
 echo "Cam8 Make MP4..." && /config/scripts/h265_to_mp4.sh ${datedir} 3629cam8
 #echo "Cam6 Archiving Oldest..." && /config/scripts/move_oldest_cam_files.sh /media/3629cam6 /share/3629cam6_old
