4b2137d6001beea399d68c47264ff2a992f7a888
Mike Loeffler <mtl010957@comcast.net>
2024-12-10 11:03:53 -0500

    Fix process of migrating special 264 file to mp4

--- scripts/h264_to_mp4.sh
+++ scripts/h264_to_mp4.sh
@@ -5,11 +5,19 @@ cam=$2
 echo ${datedir}
 echo ${cam}
 
+if ! command -v mkvmerge 2>&1 >/dev/null
+then
+    echo "mkvmerge could not be found, installing mkvtoolnix"
+    apk add mkvtoolnix
+fi
+
 cd /media/${cam}/${datedir}/record
 for infile in `ls *.264`
 do
   outfile=`basename ${infile} .264`.mp4
   wavfile=`basename ${infile} .264`.wav
+  mp3file=`basename ${infile} .264`.mp3
+  mkvfile=`basename ${infile} .264`.mkv
   h264file=`basename ${infile} .264`.h264
   audiotsfile=`basename ${infile} .264`.audio.ts.txt
   videotsfile=`basename ${infile} .264`.video.ts.txt
@@ -17,5 +25,7 @@ do
   echo ${infile}
   echo ${outfile}
   /config/scripts/conv264 ${infile}
-  ffmpeg -y -i ${wavfile} -i ${h264file} ${outfile} && rm ${infile} ${wavfile} ${h264file} ${audiotsfile} ${videotsfile}
+  ffmpeg -i ${wavfile} -y ${mp3file}
+  mkvmerge --output ${mkvfile} --timestamps "0:${videotsfile}" ${h264file} ${mp3file}
+  ffmpeg -i ${mkvfile} ${outfile} && rm ${infile} ${wavfile} ${mp3file} ${mkvfile} ${h264file} ${audiotsfile} ${videotsfile}
 done
