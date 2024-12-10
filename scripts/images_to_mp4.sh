cb5d552343aa2cac3704e3d47165e751caeb1368
Mike Loeffler <michael.t.loeffler@gmail.com>
2024-12-10 11:01:23 -0500

    Cleanup and add images movie script

---
+++ scripts/images_to_mp4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+datedir=$1
+cam=$2
+echo ${datedir}
+echo ${cam}
+
+cd /media/${cam}/${datedir}/images
+outfile=../record/${datedir}.mp4
+echo ${cam}
+echo ${outfile}
+ffmpeg -framerate 3 -pattern_type glob -i '*.jpg' -pix_fmt yuv420p -r 30 -y ${outfile}
--- scripts/special_h265_to_mp4.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/config/scripts/redo_h265_to_mp4.sh > /config/redo_h265_to_mp4.log 2>&1 &
