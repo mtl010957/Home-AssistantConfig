65f659f8a67eb24132ea1daac4bb2e5283bb61e7
Mike Loeffler <mtl010957@comcast.net>
2024-12-25 09:06:33 -0500

    Fix script to handle case of no images

--- scripts/images_to_mp4.sh
+++ scripts/images_to_mp4.sh
@@ -5,8 +5,11 @@ cam=$2
 echo ${datedir}
 echo ${cam}
 
-cd /media/${cam}/${datedir}/images
-outfile=../record/${datedir}.mp4
-echo ${cam}
-echo ${outfile}
-ffmpeg -framerate 3 -pattern_type glob -i '*.jpg' -pix_fmt yuv420p -r 30 -y ${outfile}
+if [ -d "/media/${cam}/${datedir}/images" ]
+then
+  cd /media/${cam}/${datedir}/images
+  outfile=../record/${datedir}.mp4
+  echo ${cam}
+  echo ${outfile}
+  ffmpeg -framerate 3 -pattern_type glob -i '*.jpg' -pix_fmt yuv420p -r 30 -y ${outfile}
+fi
