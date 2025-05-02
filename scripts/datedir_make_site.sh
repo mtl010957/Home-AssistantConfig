7e1e6aed211b4d7cac5ce88124f79b32a425edac
Mike Loeffler <mtl010957@comcast.net>
2025-05-02 09:07:31 -0400

    No longer need to handle H265 files

--- scripts/datedir_make_site.sh
+++ scripts/datedir_make_site.sh
@@ -12,7 +12,7 @@ echo "Cam8 Make MP4 from Images..." && /config/scripts/images_to_mp4.sh ${datedi
 echo "Cam1 Make MP4..." && /config/scripts/h264_to_mp4.sh ${datedir} 13821cam1
 echo "Cam6 Make MP4..." && /config/scripts/h264_to_mp4.sh ${datedir} 3629cam6
 echo "Cam8 Make MP4..." && /config/scripts/h264_to_mp4.sh ${datedir} 3629cam8
-echo "Cam1 Make MP4..." && /config/scripts/h265_to_mp4.sh ${datedir} 13821cam1
-echo "Cam6 Make MP4..." && /config/scripts/h265_to_mp4.sh ${datedir} 3629cam6
-echo "Cam8 Make MP4..." && /config/scripts/h265_to_mp4.sh ${datedir} 3629cam8
+#echo "Cam1 Make MP4..." && /config/scripts/h265_to_mp4.sh ${datedir} 13821cam1
+#echo "Cam6 Make MP4..." && /config/scripts/h265_to_mp4.sh ${datedir} 3629cam6
+#echo "Cam8 Make MP4..." && /config/scripts/h265_to_mp4.sh ${datedir} 3629cam8
 
